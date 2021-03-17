53f4ee9e045b3f47daa2720395db65e93a4c43a2
Aleksandr Kropanev <kropanev@mail.ru>
2021-03-17 07:33:23 +0300

    add base accounts serializer and viewset

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,7 +2,10 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .views import AccountViewSet
+
 router = DefaultRouter()
+router.register('accounts', AccountViewSet, 'account')
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from accounts.models import User
+from .serializers import AccountSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticated]
