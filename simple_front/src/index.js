43b5a178d7036c27ee080991f099c80f433e83d6
Aleksandr Kropanev <kropanev@mail.ru>
2021-04-01 06:55:57 +0300

    add fetch and axios sample code

--- simple_front/src/index.js
+++ simple_front/src/index.js
@@ -1,1 +1,20 @@
-alert("Hello World!")
+import axios from 'axios';
+
+async function getPeople(id, render) {
+  const response = await fetch(`https://swapi.dev/api/people/`);
+  const human = await response.json();
+  render(human)
+}
+
+async function getStarship(id) {
+  let response;
+  try {
+    response = await axios.get(`https://swapi.dev/api/starships/`);
+  } catch (e) {
+    return "Can't fetch data from server"
+  }
+  return response.data
+}
+
+getPeople(1, h => console.log(h));
+getStarship(2).then(data => console.log(data));
